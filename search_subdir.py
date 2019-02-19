49d7a42ae61f8d947266a565cedd30dc36decf9e
jogates <billgates365@gmail.com>
2019-02-19 16:15:12 +0900

    search_subdir 변경

--- search_subdir.py
+++ search_subdir.py
@@ -1,10 +1,14 @@
 import os
-class eHiMart():
-    name = 'eHiMart'
-    url = ['https://secure.e-himart.co.kr/app/goods/goodsDetail?goodsNo=0001444858']
-    # def parse(self, response):
+def searh_file(start_path):
+    for dir_path, subdir_list, file_list in os.walk(start_path):
+        print(dir_path)
+        print(subdir_list)
+        print(file_list)
+        print('-' * 100)
+        #for fname in file_list:
+        #    print(os.path.join(file_list, fname))
 
 def main():
-    eHiMart()
+    searh_file('C:/work/My_Program/')
 
 if __name__ == '__main__': main()
